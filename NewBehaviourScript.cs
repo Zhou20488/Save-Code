c8928c90a4ce8edfa4de5ace81487a3119e9d45e
Zhou20488 <116442906+Zhou20488@users.noreply.github.com>
2023-01-01 15:23:16 +0800

    Add files via upload

---
+++ NewBehaviourScript.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Iten",menuName ="Invotry/New Iten")]
+public class NewBehaviourScript : ScriptableObject
+{
+    public int Leven;
+    public int iswd;
+    public int number;
+    public GameObject player;
+}
